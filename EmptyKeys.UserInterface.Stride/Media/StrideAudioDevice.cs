eb2e5e073ec1ad35946fab3513841a03365c9aa7
Doprez <73259914+Doprez@users.noreply.github.com>
2025-05-21 17:50:15 -0600

    added Stride package

---
+++ EmptyKeys.UserInterface.Stride/Media/StrideAudioDevice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyKeys.UserInterface.Media
+{
+    /// <summary>
+    /// Implements Stride specific audio device
+    /// </summary>
+    public class StrideAudioDevice : AudioDevice
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StrideAudioDevice"/> class.
+        /// </summary>
+        public StrideAudioDevice()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Creates the sound.
+        /// </summary>
+        /// <param name="nativeSound">The native sound.</param>
+        /// <returns></returns>
+        public override SoundBase CreateSound(object nativeSound)
+        {
+            return new StrideSound(nativeSound);
+        }
+    }
+}
